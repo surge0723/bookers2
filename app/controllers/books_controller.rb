d89dd447194cd4b1d575d9f10225e24acca0f9c2
surge0723 <170217595+surge0723@users.noreply.githuglobalb.com>
2024-07-29 02:46:19 +0000

    難航中…

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,30 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def index
+    @books = Book.all
   end
 
   def show
+        @book = Book.find(params[:id])
+  end
+  
+    private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'homes/top'
     root to: "homes#top"
-    resources:users,only: [:index,:show]
-    resources:books,only: [:index,:show]
+    resources:users,only: [:index,:show, :destroy]
+    resources:books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
